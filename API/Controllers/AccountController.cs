f85fcedd5d3d11278f9c0da1523b98f76c4a6b24
Ali Chavoshi <alichavoshii1372@gmail.com>
2021-10-24 12:47:17 +0330

    4-Dto-Data Transfer Object

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,7 +2,9 @@ using System.Security.Cryptography;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -15,18 +17,26 @@ namespace API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<Users>> Register(string userName, string password)
+        public async Task<ActionResult<Users>> Register(RegisterDto model)
         {
+            if (await IsExistUserName(model.userName))
+                return BadRequest("نام کاربری موجود میباشد");
+
             using var hmac = new HMACSHA512();
             var user = new Users
             {
-                UserName = userName,
+                UserName = model.userName,
                 PasswordSalt = hmac.Key,
-                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password)),
+                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(model.Password)),
             };
             await _context.Users.AddAsync(user);
             await _context.SaveChangesAsync();
             return user;
+        }
+
+        private async Task<bool> IsExistUserName(string userName)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName.ToLower() == userName.ToLower());
         }
     }
 }
---
+++ API/Models/RegisterDto.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class RegisterDto
+    {
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد نمائید")]
+        [MaxLength(50, ErrorMessage = "بیشتری کارکتر 50 میباشد")]
+        [MinLength(3, ErrorMessage = "کمترین کاراکتر 3 میباشد ")]
+        public string userName { get; set; }
+
+        [Display(Name = "گذرواژه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد نمائید")]
+        [MaxLength(20, ErrorMessage = "بیشتری کارکتر 20 میباشد")]
+        [MinLength(5, ErrorMessage = "کمترین کاراکتر 5 میباشد ")]
+        public string Password { get; set; }
+    }
+}
