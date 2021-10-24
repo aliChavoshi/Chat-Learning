5171fb0323d08286d154fd5809569811cf53219b
Ali Chavoshi <alichavoshii1372@gmail.com>
2021-10-24 13:06:19 +0330

    5-adding a login endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -34,6 +34,21 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<Users>> Login([FromBody] LoginDto model)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName.ToLower() == model.userName.ToLower());
+            if (user == null) return BadRequest("نام کاربری یافت نشد");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(model.Password));
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return BadRequest("کلمه عبور اشتباه است");
+            }
+            return user;
+        }
+
         private async Task<bool> IsExistUserName(string userName)
         {
             return await _context.Users.AnyAsync(x => x.UserName.ToLower() == userName.ToLower());
