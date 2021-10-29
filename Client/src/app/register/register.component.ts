7c5855a24f0164c9f01ddfb4339359d4fd1779e3
Ali Chavoshi <alichavoshii1372@gmail.com>
2021-10-29 16:50:58 +0330

    8-adding register component

--- Client/src/app/register/register.component.ts
+++ Client/src/app/register/register.component.ts
@@ -1,15 +1,34 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  form = new FormGroup({
+    userName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(50),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(20),
+    ]),
+    passwordConfirm: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(20),
+    ]),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onSubmit() {}
+  cancel() {}
 }
