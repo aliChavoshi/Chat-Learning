c78d62c95588509add1814fd551f25f577d9546e
Ali Chavoshi <alichavoshii1372@gmail.com>
2021-11-17 21:17:35 +0330

    8-Add Seed Data To Program.cs

--- API/Data/SeedData/SeedUserData.cs
+++ API/Data/SeedData/SeedUserData.cs
@@ -8,27 +8,36 @@ using Microsoft.EntityFrameworkCore;
 using System.Text.Json;
 using API.Entities;
 using System.Text;
+using Microsoft.Extensions.Logging;
 
 namespace API.Data.SeedData
 {
     public class SeedUserData
     {
-        public static async Task SeedUsers(DataContext context)
+        public static async Task SeedUsers(DataContext context, ILoggerFactory logger)
         {
-            if (!await context.Users.AnyAsync())
+            try
             {
-                var userData = await File.ReadAllTextAsync("Data/SeedData/UserSeedData.json");
-                var users = JsonSerializer.Deserialize<List<Users>>(userData);
-                if (users == null) return;
-                foreach (var user in users)
+                if (!await context.Users.AnyAsync())
                 {
-                    using var hmac = new HMACSHA512();
-                    user.UserName = user.UserName.ToLower();
-                    user.PasswordSalt = hmac.Key;
-                    user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
+                    var userData = await File.ReadAllTextAsync("Data/SeedData/UserSeedData.json");
+                    var users = JsonSerializer.Deserialize<List<Users>>(userData);
+                    if (users == null) return;
+                    foreach (var user in users)
+                    {
+                        using var hmac = new HMACSHA512();
+                        user.UserName = user.UserName.ToLower();
+                        user.PasswordSalt = hmac.Key;
+                        user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
+                    }
+                    await context.Users.AddRangeAsync(users);
+                    await context.SaveChangesAsync();
                 }
-                await context.Users.AddRangeAsync(users);
-                await context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                var log = logger.CreateLogger<SeedUserData>();
+                log.LogError(ex.Message);
             }
         }
     }
--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,39 @@
+using System.Runtime.InteropServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using API.Data.SeedData;
 
 namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+                await context.Database.MigrateAsync();
+                await SeedUserData.SeedUsers(context, loggerFactory);
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(ex.Message);
+            }
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
