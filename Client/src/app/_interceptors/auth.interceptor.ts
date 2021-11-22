482e6b3c2d035bd4cb301d104b13ef47c082ce7c
Ali Chavoshi <alichavoshii1372@gmail.com>
2021-11-22 22:58:58 +0330

    8-exhuastMap Operator

--- Client/src/app/_interceptors/auth.interceptor.ts
+++ Client/src/app/_interceptors/auth.interceptor.ts
@@ -10,29 +10,29 @@ import {
 import { Observable } from 'rxjs';
 import { AccountService } from '../_services/account.service';
 import { User } from '../_models/account';
-import { take, tap } from 'rxjs/operators';
+import { take, tap, exhaustMap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  currentUser: User;
   constructor(private accountService: AccountService) {}
 
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    this.accountService.currentUser$.pipe(take(1)).subscribe((user) => {
-      this.currentUser = user;
-    });
-    if (this.currentUser) {
-      request = request.clone({
-        headers: new HttpHeaders().set(
-          'Authorization',
-          'Bearer ' + this.currentUser.token
-        ),
-      });
-    }
-    return next.handle(request).pipe(
+    return this.accountService.currentUser$.pipe(
+      take(1),
+      exhaustMap((user) => {
+        if (user) {
+          request = request.clone({
+            headers: new HttpHeaders().set(
+              'Authorization',
+              'Bearer ' + user?.token
+            ),
+          });
+        }
+        return next.handle(request);
+      }),
       tap((event) => {
         if (event.type === HttpEventType.Sent) {
         }
