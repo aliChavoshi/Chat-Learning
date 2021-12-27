53fbb8aade7db528162aaa1ce6933ddf050df470
Ali Chavoshi <alichavoshii1372@gmail.com>
2021-12-27 18:58:15 +0330

    1-Add Paged List Class

--- API/Helpers/PagedList.cs
+++ API/Helpers/PagedList.cs
@@ -1,12 +1,31 @@
+using System.Reflection.Metadata.Ecma335;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Helpers
 {
-    public class PagedList<T>
+    public class PagedList<T> : List<T>
     {
-        
+        public PagedList(IEnumerable<T> items, int pageNumber, int pageSize, int count)
+        {
+            this.TotalPage = (int)Math.Ceiling(count / (double)pageSize); // 20/10 = 2;
+            this.AddRange(items);
+            this.CurrentPage = pageNumber;
+            this.PageSize = pageSize;
+            this.TotalCount = count;
+        }
+        public int CurrentPage { get; set; }
+        public int TotalPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PagedList<T>(items, pageNumber, pageSize, count);
+        }
     }
 }
