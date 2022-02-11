b83180730f8eb225474b343f1e6b037d276a5e84
Ali Chavoshi <alichavoshii1372@gmail.com>
2022-02-11 15:00:28 +0330

    7-Designing the inbox2

--- Client/src/app/_models/message.ts
+++ Client/src/app/_models/message.ts
@@ -1,18 +1,21 @@
 export interface IMessage {
   id: number;
+  //send
   senderId: number;
   senderUserName: string;
   senderPhotoUrl: string;
+  //receiver
   receiverId: number;
   receiverUserName: string;
   receiverPhotoUrl: string;
+  //
   content: string;
   dateRead: Date;
-  messageSent: string;
+  messageSent: Date;
   isRead: boolean;
 }
 export class MessageParams {
-  container: string = 'Inbox';
+  container: string = 'Inbox'; //Outbox , Unread , Inbox
   pageNumber: number = 1;
   pageSize: number = 10;
 }
--- Client/src/app/messages/home-message/home-message.component.ts
+++ Client/src/app/messages/home-message/home-message.component.ts
@@ -2,6 +2,7 @@ import { PaginatedResult } from 'src/app/_models/pagination';
 import { IMessage, MessageParams } from './../../_models/message';
 import { MessageService } from './../../_services/message.service';
 import { Component, OnInit } from '@angular/core';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-home-message',
@@ -11,6 +12,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeMessageComponent implements OnInit {
   messageParams: MessageParams;
   result: PaginatedResult<IMessage[]>;
+  loading = false;
 
   constructor(private messageService: MessageService) {
     this.messageParams = this.messageService.getMessageParams();
@@ -20,11 +22,18 @@ export class HomeMessageComponent implements OnInit {
     this.loadMessages();
   }
   loadMessages() {
+    this.loading = true;
     this.messageService
       .getMessages(this.messageParams)
+      .pipe(finalize(() => (this.loading = false)))
       .subscribe((response) => {
         this.result = response;
       });
+  }
+  changeContainer(container: string) {
+    this.messageParams.container = container;
+    this.messageService.setMessageParams(this.messageParams);
+    this.loadMessages();
   }
   pageChanged(event: any): void {
     this.messageParams.pageNumber = event.page;
